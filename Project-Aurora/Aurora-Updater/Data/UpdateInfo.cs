dd71a23c2eb6af5fcb912c60089656d389156738
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-03-31 23:55:45 +0200

    fix pre-release check always checking for v199

--- Project-Aurora/Aurora-Updater/Data/UpdateInfo.cs
+++ Project-Aurora/Aurora-Updater/Data/UpdateInfo.cs
@@ -8,11 +8,10 @@ namespace Aurora_Updater.Data;
 public class UpdateInfo(Version currentVersion, string author, string repoName, bool getPreReleases)
 {
     private readonly GitHubClient _gClient = new(new ProductHeaderValue("aurora-updater", currentVersion.ToString()));
-    private readonly Version _currentVersion = new(199, 0, 0);
 
     public async Task<Release> FetchData()
     {
-        if (!getPreReleases && string.IsNullOrWhiteSpace(_currentVersion.PreRelease) && !await IsCurrentlyPreRelease())
+        if (!getPreReleases && string.IsNullOrWhiteSpace(currentVersion.PreRelease) && !await IsCurrentlyPreRelease())
         {
             return await _gClient.Repository.Release.GetLatest(author, repoName);
         }
@@ -23,7 +22,14 @@ public class UpdateInfo(Version currentVersion, string author, string repoName, 
 
     private async Task<bool> IsCurrentlyPreRelease()
     {
-        var release = await _gClient.Repository.Release.Get(author, repoName, $"v{_currentVersion.Major}");
-        return release.Prerelease;
+        try
+        {
+            var release = await _gClient.Repository.Release.Get(author, repoName, $"v{currentVersion.Major}");
+            return release.Prerelease;
+        }
+        catch
+        {
+            return false;
+        }
     }
 }
