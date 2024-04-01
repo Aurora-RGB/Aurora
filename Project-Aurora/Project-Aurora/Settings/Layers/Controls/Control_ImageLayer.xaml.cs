c9332194858ed3b7a361a46da0cbec394a302b18
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-04-01 21:55:45 +0200

    optimized image layer

--- Project-Aurora/Project-Aurora/Settings/Layers/Controls/Control_ImageLayer.xaml.cs
+++ Project-Aurora/Project-Aurora/Settings/Layers/Controls/Control_ImageLayer.xaml.cs
@@ -1,27 +1,26 @@
 ﻿using System.IO;
 using System.Windows;
 using System.Windows.Forms;
-using UserControl = System.Windows.Controls.UserControl;
 
-namespace AuroraRgb.Settings.Layers.Controls {
+namespace AuroraRgb.Settings.Layers.Controls;
 
-    public partial class Control_ImageLayer : UserControl {
-        public Control_ImageLayer() {
-            InitializeComponent();
-        }
+public partial class Control_ImageLayer
+{
+    public Control_ImageLayer() {
+        InitializeComponent();
+    }
 
-        public Control_ImageLayer(ImageLayerHandler context) {
-            InitializeComponent();
-            DataContext = context;
-        }
+    public Control_ImageLayer(ImageLayerHandler context) {
+        InitializeComponent();
+        DataContext = context;
+    }
 
-        private void SelectImage_Click(object? sender, RoutedEventArgs e) {
-            OpenFileDialog dialog = new OpenFileDialog {
-                Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.png, *.gif, *.bmp, *.tiff, *.tif) | *.jpg; *.jpeg; *.jpe; *.png; *.gif; *.bmp; *.tiff; *.tif",
-                Title = "Please select an image."
-            };
-            if (dialog.ShowDialog() == DialogResult.OK && File.Exists(dialog.FileName))
-                (this.DataContext as ImageLayerHandler).Properties._ImagePath = dialog.FileName;
-        }
+    private void SelectImage_Click(object? sender, RoutedEventArgs e) {
+        var dialog = new OpenFileDialog {
+            Filter = "Image files (*.jpg, *.jpeg, *.jpe, *.png, *.gif, *.bmp, *.tiff, *.tif) | *.jpg; *.jpeg; *.jpe; *.png; *.gif; *.bmp; *.tiff; *.tif",
+            Title = "Please select an image."
+        };
+        if (dialog.ShowDialog() == DialogResult.OK && File.Exists(dialog.FileName))
+            ((ImageLayerHandler)DataContext).Properties.ImagePath = dialog.FileName;
     }
 }
--- Project-Aurora/Project-Aurora/Settings/Layers/ImageLayerHandler.cs
+++ Project-Aurora/Project-Aurora/Settings/Layers/ImageLayerHandler.cs
@@ -1,119 +1,100 @@
-﻿using System;
+﻿using System.ComponentModel;
 using System.Drawing;
-using System.Drawing.Drawing2D;
 using System.IO;
 using System.Windows.Controls;
 using AuroraRgb.EffectsEngine;
 using AuroraRgb.Profiles;
 using AuroraRgb.Settings.Layers.Controls;
 using AuroraRgb.Settings.Overrides;
-using Common.Utils;
 using Newtonsoft.Json;
+using Image = System.Drawing.Image;
 
-namespace AuroraRgb.Settings.Layers
+namespace AuroraRgb.Settings.Layers;
+
+public class ImageLayerHandlerProperties : LayerHandlerProperties2Color<ImageLayerHandlerProperties>
 {
-    public class ImageLayerHandlerProperties : LayerHandlerProperties2Color<ImageLayerHandlerProperties>
+    private string? _imagePath;
+
+    [JsonProperty("_ImagePath")]
+    public string? ImagePath
     {
-        public string _ImagePath { get; set; }
-
-        [JsonIgnore]
-        public string ImagePath { get { return Logic._ImagePath ?? _ImagePath; } }
-
-        public ImageLayerHandlerProperties()
-        { }
-
-        public ImageLayerHandlerProperties(bool assign_default = false) : base(assign_default) { }
-
-        public override void Default()
-        {
-            base.Default();
-            this._ImagePath = "";
-        }
+        get => Logic._imagePath ?? _imagePath;
+        set => _imagePath = value;
     }
 
-    [LogicOverrideIgnoreProperty("_PrimaryColor")]
-    [LogicOverrideIgnoreProperty("_SecondaryColor")]
-    public class ImageLayerHandler : LayerHandler<ImageLayerHandlerProperties>
+    public ImageLayerHandlerProperties()
+    { }
+
+    public ImageLayerHandlerProperties(bool assign_default = false) : base(assign_default) { }
+
+    public override void Default()
     {
-        private EffectLayer temp_layer;
-        private System.Drawing.Image _loaded_image = null;
-        private string _loaded_image_path = "";
+        base.Default();
+        _imagePath = "";
+    }
+}
 
-        public ImageLayerHandler(): base("ImageLayer")
+[LogicOverrideIgnoreProperty("_PrimaryColor")]
+[LogicOverrideIgnoreProperty("_SecondaryColor")]
+public class ImageLayerHandler() : LayerHandler<ImageLayerHandlerProperties>("ImageLayer")
+{
+    private Image? _loadedImage;
+    private string? _loadedImagePath = "";
+    private bool _invalidated = true;
+
+    protected override UserControl CreateControl()
+    {
+        return new Control_ImageLayer(this);
+    }
+
+    public override EffectLayer Render(IGameState gamestate)
+    {
+        if (string.IsNullOrWhiteSpace(Properties.ImagePath)) return EffectLayer.EmptyLayer;
+        
+        if (_loadedImagePath != Properties.ImagePath)
         {
+            //Not loaded, load it!
+            if (!File.Exists(Properties.ImagePath))
+                return EffectLayer.EmptyLayer;
+
+            _loadedImage?.Dispose();
+            _loadedImage = new Bitmap(Properties.ImagePath);
+            _loadedImagePath = Properties.ImagePath;
+
+            _invalidated = true;
+
+            if (Properties.ImagePath.EndsWith(".gif") && ImageAnimator.CanAnimate(_loadedImage))
+                ImageAnimator.Animate(_loadedImage, null);
         }
 
-        protected override UserControl CreateControl()
+        if (Properties.ImagePath.EndsWith(".gif") && ImageAnimator.CanAnimate(_loadedImage))
         {
-            return new Control_ImageLayer(this);
+            ImageAnimator.UpdateFrames(_loadedImage);
+            _invalidated = true;
         }
 
-        public override EffectLayer Render(IGameState gamestate)
+        if (_invalidated)
         {
-            var image_layer = new EffectLayer("ImageLayer");
-
-            if (!String.IsNullOrWhiteSpace(Properties.ImagePath))
+            EffectLayer.Clear();
+        }
+        
+        EffectLayer.DrawTransformed(Properties.Sequence,
+            _ => {},
+            g =>
             {
+                g.DrawImage(_loadedImage, 0, 0);
+            },
+            new RectangleF(0, 0, _loadedImage.Width, _loadedImage.Height)
+        );
 
-                if (!_loaded_image_path.Equals(Properties.ImagePath))
-                {
-                    //Not loaded, load it!
-                    if (!File.Exists(Properties.ImagePath))
-                        throw new FileNotFoundException("Could not find file specified for layer: " + Properties.ImagePath);
+        _invalidated = false;
+        return EffectLayer;
+    }
 
-                    _loaded_image = new Bitmap(Properties.ImagePath);
-                    _loaded_image_path = Properties.ImagePath;
+    protected override void PropertiesChanged(object? sender, PropertyChangedEventArgs args)
+    {
+        base.PropertiesChanged(sender, args);
 
-                    if (Properties.ImagePath.EndsWith(".gif") && ImageAnimator.CanAnimate(_loaded_image))
-                        ImageAnimator.Animate(_loaded_image, null);
-                }
-
-                if (Properties.ImagePath.EndsWith(".gif") && ImageAnimator.CanAnimate(_loaded_image))
-                    ImageAnimator.UpdateFrames(_loaded_image);
-
-                temp_layer = new EffectLayer("Temp Image Render");
-
-                if (Properties.Sequence.Type == KeySequenceType.Sequence)
-                {
-                    using (var g = temp_layer.GetGraphics())
-                    {
-                        g.DrawImage(_loaded_image, new RectangleF(0, 0, Effects.Canvas.Width, Effects.Canvas.Height), new RectangleF(0, 0, _loaded_image.Width, _loaded_image.Height), GraphicsUnit.Pixel);
-                    }
-
-                    foreach (var key in Properties.Sequence.Keys)
-                        image_layer.Set(key, CommonColorUtils.AddColors(image_layer.Get(key), temp_layer.Get(key)));
-                }
-                else
-                {
-                    var x_pos = (float)Math.Round((Properties.Sequence.Freeform.X + Effects.Canvas.GridBaselineX) * Effects.Canvas.EditorToCanvasWidth);
-                    var y_pos = (float)Math.Round((Properties.Sequence.Freeform.Y + Effects.Canvas.GridBaselineY) * Effects.Canvas.EditorToCanvasHeight);
-                    var width = (float)Math.Round((double)(Properties.Sequence.Freeform.Width * Effects.Canvas.EditorToCanvasWidth));
-                    var height = (float)Math.Round((double)(Properties.Sequence.Freeform.Height * Effects.Canvas.EditorToCanvasHeight));
-
-                    if (width < 3) width = 3;
-                    if (height < 3) height = 3;
-
-                    var rect = new Rectangle((int)x_pos, (int)y_pos, (int)width, (int)height);
-
-                    using (var g = temp_layer.GetGraphics())
-                    {
-                        g.DrawImage(_loaded_image, rect, new RectangleF(0, 0, _loaded_image.Width, _loaded_image.Height), GraphicsUnit.Pixel);
-                    }
-
-                    using (var g = image_layer.GetGraphics())
-                    {
-                        var rotatePoint = new PointF(x_pos + (width / 2.0f), y_pos + (height / 2.0f));
-
-                        var myMatrix = new Matrix();
-                        myMatrix.RotateAt(Properties.Sequence.Freeform.Angle, rotatePoint, MatrixOrder.Append);
-
-                        g.Transform = myMatrix;
-                        g.DrawImage(temp_layer.GetBitmap(), rect, rect, GraphicsUnit.Pixel);
-                    }
-                }
-            }
-
-            return image_layer;
-        }
+        _invalidated = true;
     }
 }
