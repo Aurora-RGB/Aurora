b06e66917c94fbb12bf94001ce66dde524313701
Aytaç Kayadelen <aytackayadelen@gmail.com>
2024-04-06 05:08:32 +0200

    optimize Timer Layer

--- Project-Aurora/Project-Aurora/Settings/Layers/Controls/Control_TimerLayer.xaml.cs
+++ Project-Aurora/Project-Aurora/Settings/Layers/Controls/Control_TimerLayer.xaml.cs
@@ -1,20 +1,18 @@
-﻿using System.Windows.Controls;
-using AuroraRgb.Controls;
+﻿using AuroraRgb.Controls;
 
-namespace AuroraRgb.Settings.Layers.Controls {
+namespace AuroraRgb.Settings.Layers.Controls;
 
-    public partial class Control_TimerLayer : UserControl {
+public partial class Control_TimerLayer
+{
+    public Control_TimerLayer(TimerLayerHandler context) {
+        InitializeComponent();
+        DataContext = context;
 
-        public Control_TimerLayer(TimerLayerHandler context) {
-            InitializeComponent();
-            DataContext = context;
+        triggerKeyList.Keybinds = context.Properties.TriggerKeys;
+    }
 
-            triggerKeyList.Keybinds = context.Properties._TriggerKeys;
-        }
-
-        private void triggerKeyList_KeybindsChanged(object? sender) {
-            if (sender is KeyBindList kbl)
-                (DataContext as TimerLayerHandler).Properties._TriggerKeys = kbl.Keybinds;
-        }
+    private void triggerKeyList_KeybindsChanged(object? sender) {
+        if (sender is KeyBindList kbl && DataContext is TimerLayerHandler timerLayerHandler)
+            timerLayerHandler.Properties.TriggerKeys = kbl.Keybinds;
     }
 }
--- Project-Aurora/Project-Aurora/Settings/Layers/TimerLayerHandler.cs
+++ Project-Aurora/Project-Aurora/Settings/Layers/TimerLayerHandler.cs
@@ -5,208 +5,246 @@ using System.Timers;
 using System.Windows.Controls;
 using AuroraRgb.EffectsEngine;
 using AuroraRgb.Modules;
+using AuroraRgb.Modules.Inputs;
 using AuroraRgb.Profiles;
 using AuroraRgb.Settings.Layers.Controls;
 using AuroraRgb.Settings.Overrides;
 using AuroraRgb.Utils;
 using Newtonsoft.Json;
 
-namespace AuroraRgb.Settings.Layers {
+namespace AuroraRgb.Settings.Layers;
 
-    public sealed class TimerLayerHandlerProperties : LayerHandlerProperties2Color<TimerLayerHandlerProperties> {
+public sealed class TimerLayerHandlerProperties : LayerHandlerProperties2Color<TimerLayerHandlerProperties> {
 
-        public TimerLayerHandlerProperties()
-        { }
-        public TimerLayerHandlerProperties(bool assign_default) : base(assign_default) { }
+    public TimerLayerHandlerProperties()
+    { }
+    public TimerLayerHandlerProperties(bool assignDefault) : base(assignDefault) { }
 
-        public Keybind[]? _TriggerKeys { get; set; }
-        [JsonIgnore]
-        public Keybind[] TriggerKeys { get { return Logic._TriggerKeys ?? _TriggerKeys ?? []; } }
-
-        [LogicOverridable("Duration")]
-        public int? _Duration { get; set; }
-        [JsonIgnore]
-        public int Duration { get { return Logic._Duration ?? _Duration ?? 0; } }
-
-        [LogicOverridable("Animation Type")]
-        public TimerLayerAnimationType? _AnimationType { get; set; }
-        [JsonIgnore]
-        public TimerLayerAnimationType AnimationType { get { return Logic._AnimationType ?? _AnimationType ?? TimerLayerAnimationType.OnOff; } }
-
-        [LogicOverridable("Repeat Action")]
-        public TimerLayerRepeatPressAction? _RepeatAction { get; set; }
-        [JsonIgnore]
-        public TimerLayerRepeatPressAction RepeatAction { get { return Logic._RepeatAction ?? _RepeatAction ?? TimerLayerRepeatPressAction.Reset; } }
-
-        public override void Default() {
-            base.Default();
-            _TriggerKeys = new Keybind[] { };
-            _Duration = 5000;
-            _AnimationType = TimerLayerAnimationType.OnOff;
-            _RepeatAction = TimerLayerRepeatPressAction.Reset;
-        }
+    private Keybind[]? _triggerKeys;
+    [JsonProperty("_TriggerKeys")]
+    public Keybind[] TriggerKeys
+    {
+        get => Logic._triggerKeys ?? _triggerKeys ?? [];
+        set => _triggerKeys = value;
     }
 
-    public class TimerLayerHandler : LayerHandler<TimerLayerHandlerProperties> {
+    private int? _duration;
+    [LogicOverridable("Duration")]
+    [JsonProperty("_Duration")]
+    public int Duration
+    {
+        get => Logic._duration ?? _duration ?? 0;
+        set => _duration = value;
+    }
 
-        private CustomTimer timer;
-        private bool isActive;
+    private TimerLayerAnimationType? _animationType;
+    [JsonProperty("_AnimationType")]
+    [LogicOverridable("Animation Type")]
+    public TimerLayerAnimationType AnimationType
+    {
+        get => Logic._animationType ?? _animationType ?? TimerLayerAnimationType.OnOff;
+        set => _animationType = value;
+    }
 
-        public TimerLayerHandler()
+    private TimerLayerRepeatPressAction? _repeatAction;
+    [JsonProperty("_RepeatAction")]
+    [LogicOverridable("Repeat Action")]
+    public TimerLayerRepeatPressAction RepeatAction
+    {
+        get => Logic._repeatAction ?? _repeatAction ?? TimerLayerRepeatPressAction.Reset;
+        set => _repeatAction = value;
+    }
+
+    public override void Default() {
+        base.Default();
+        _triggerKeys = [];
+        _duration = 5000;
+        _animationType = TimerLayerAnimationType.OnOff;
+        _repeatAction = TimerLayerRepeatPressAction.Reset;
+    }
+}
+
+public class TimerLayerHandler : LayerHandler<TimerLayerHandlerProperties> {
+    private readonly CustomTimer _timer;
+    private bool _isActive;
+    private bool _invalidated;
+
+    public TimerLayerHandler(): base("Timer Layer")
+    {
+        _timer = new CustomTimer();
+        _timer.Trigger += Timer_Elapsed;
+    }
+
+    protected override async Task Initialize()
+    {
+        await base.Initialize();
+
+        (await InputsModule.InputEvents).KeyDown += InputEvents_KeyDown;
+    }
+
+    public override void Dispose() {
+        InputsModule.InputEvents.Result.KeyDown -= InputEvents_KeyDown;
+        _timer.Dispose();
+        base.Dispose();
+    }
+
+    protected override UserControl CreateControl() {
+        return new Control_TimerLayer(this);
+    }
+
+    public override EffectLayer Render(IGameState gameState) {
+        if (_invalidated)
         {
-            timer = new CustomTimer();
-            timer.Trigger += Timer_Elapsed;
+            EffectLayer.Clear();
         }
 
-        protected override async Task Initialize()
+        if (!_isActive)
         {
-            await base.Initialize();
-
-            (await InputsModule.InputEvents).KeyDown += InputEvents_KeyDown;
+            EffectLayer.Set(Properties.Sequence, Properties.PrimaryColor);
+            return EffectLayer;
         }
 
-        public override void Dispose() {
-            InputsModule.InputEvents.Result.KeyDown -= InputEvents_KeyDown;
-            base.Dispose();
+        switch (Properties.AnimationType)
+        {
+            case TimerLayerAnimationType.OnOff:
+                EffectLayer.Set(Properties.Sequence, Properties.SecondaryColor);
+                break;
+
+            case TimerLayerAnimationType.Fade:
+                EffectLayer.Set(Properties.Sequence,
+                    ColorUtils.BlendColors(Properties.SecondaryColor, Properties.PrimaryColor, _timer.InterpolationValue));
+                break;
+
+            case TimerLayerAnimationType.Progress:
+            case TimerLayerAnimationType.ProgressGradual:
+                EffectLayer.PercentEffect(Properties.SecondaryColor, Properties.PrimaryColor, Properties.Sequence,
+                    _timer.InterpolationValue, 1,
+                    Properties.AnimationType == TimerLayerAnimationType.Progress
+                        ? PercentEffectType.Progressive
+                        : PercentEffectType.Progressive_Gradual);
+                break;
         }
 
-        protected override UserControl CreateControl() {
-            return new Control_TimerLayer(this);
+        return EffectLayer;
+    }
+
+    protected override void PropertiesChanged(object? sender, PropertyChangedEventArgs args)
+    {
+        base.PropertiesChanged(sender, args);
+
+        _invalidated = true;
+    }
+
+    private void Timer_Elapsed(object? sender) {
+        _isActive = false;
+    }
+
+    private void InputEvents_KeyDown(object? sender, KeyboardKeyEventArgs e)
+    {
+        if (!Array.Exists(Properties.TriggerKeys, keybind => keybind.IsPressed()))
+        {
+            return;
         }
 
-        public override EffectLayer Render(IGameState gamestate) {
-            EffectLayer layer = new EffectLayer("TimerLayer");
-            if (isActive) {
-                switch (Properties.AnimationType) {
-                    case TimerLayerAnimationType.OnOff:
-                        layer.Set(Properties.Sequence, Properties.SecondaryColor);
-                        break;
+        switch (Properties.RepeatAction) {
+            // Restart the timer from scratch
+            case TimerLayerRepeatPressAction.Reset:
+                _timer.Reset(Properties.Duration);
+                _isActive = true;
+                break;
 
-                    case TimerLayerAnimationType.Fade:
-                        layer.Set(Properties.Sequence, ColorUtils.BlendColors(Properties.SecondaryColor, Properties.PrimaryColor, timer.InterpolationValue));
-                        break;
+            case TimerLayerRepeatPressAction.Extend:
+                _timer.Extend(Properties.Duration);
+                _isActive = true;
+                break;
 
-                    case TimerLayerAnimationType.Progress:
-                    case TimerLayerAnimationType.ProgressGradual:
-                        layer.PercentEffect(Properties.SecondaryColor, Properties.PrimaryColor, Properties.Sequence, timer.InterpolationValue, 1, Properties.AnimationType == TimerLayerAnimationType.Progress ? PercentEffectType.Progressive : PercentEffectType.Progressive_Gradual);
-                        break;
-                }
-            } else
-                layer.Set(Properties.Sequence, Properties.PrimaryColor);
-            return layer;
-        }
-
-        private void Timer_Elapsed(object? sender) {
-            isActive = false;
-        }
-
-        private void InputEvents_KeyDown(object? sender, EventArgs e) {
-            foreach (var keybind in Properties.TriggerKeys) {
-                if (keybind.IsPressed()) {
-                    switch (Properties.RepeatAction) {
-                        // Restart the timer from scratch
-                        case TimerLayerRepeatPressAction.Reset:
-                            timer.Reset(Properties.Duration);
-                            isActive = true;
-                            break;
-
-                        case TimerLayerRepeatPressAction.Extend:
-                            timer.Extend(Properties.Duration);
-                            isActive = true;
-                            break;
-
-                        case TimerLayerRepeatPressAction.Cancel:
-                            if (isActive)
-                                timer.Stop();
-                            else
-                                timer.Reset(Properties.Duration);
-                            isActive = !isActive;
-                            break;
+            case TimerLayerRepeatPressAction.Cancel:
+                if (_isActive)
+                    _timer.Stop();
+                else
+                    _timer.Reset(Properties.Duration);
+                _isActive = !_isActive;
+                break;
                         
-                        case TimerLayerRepeatPressAction.Ignore:
-                            if (!isActive) {
-                                timer.Reset(Properties.Duration);
-                                isActive = true;
-                            }
-                            break;
-                    }
-                    return;
+            case TimerLayerRepeatPressAction.Ignore:
+                if (!_isActive) {
+                    _timer.Reset(Properties.Duration);
+                    _isActive = true;
                 }
-            }
+                break;
+        }
+    }
+}
+
+internal sealed class CustomTimer : IDisposable {
+    public delegate void TriggerHandler(object? sender);
+    public event TriggerHandler? Trigger;
+
+    private readonly Timer _timer;
+    private double _max;
+    private DateTime _startAt;
+
+    public CustomTimer() {
+        _timer = new Timer();
+        _timer.AutoReset = false;
+        _timer.Elapsed += Timer_Elapsed;
+    }
+
+    private void Timer_Elapsed(object? sender, ElapsedEventArgs e) {
+        Trigger?.Invoke(this);
+        _timer.Enabled = false;
+    }
+
+    /// <summary>Stops the timer and restarts it with the given time.</summary>
+    private void SetTimer(double t) {
+        _timer.Stop();
+        _timer.Interval = t;
+        _timer.Start();
+    }
+
+    public void Reset(double t) {
+        SetTimer(t);
+        _startAt = DateTime.UtcNow;
+        _max = t;
+    }
+
+    public void Stop() {
+        _timer.Stop();
+    }
+
+    public void Extend(double t) {
+        // If the timer's not running, behave like Reset
+        if (!_timer.Enabled)
+            Reset(t);
+
+        // If timer is running
+        else {
+            _max += t; // extend max
+            SetTimer(_max - Current);
         }
     }
 
-    class CustomTimer {
+    /// <summary>Gets how many milliseconds has elapsed since starting timer.</summary>
+    public int Current => (int)(DateTime.UtcNow - _startAt).TotalMilliseconds;
 
-        public delegate void TriggerHandler(object? sender);
-        public event TriggerHandler Trigger;
+    /// <summary>Gets how far through the timer is as a value between 0 and 1 (for use with the fade animation mode).</summary>
+    public double InterpolationValue => Current / _max;
 
-        private Timer timer;
-        private double max = 0;
-        private DateTime startAt;
-
-        public CustomTimer() {
-            timer = new Timer();
-            timer.AutoReset = false;
-            timer.Elapsed += Timer_Elapsed;
-        }
-
-        private void Timer_Elapsed(object? sender, ElapsedEventArgs e) {
-            Trigger?.Invoke(this);
-            timer.Enabled = false;
-        }
-
-        /// <summary>Stops the timer and restarts it with the given time.</summary>
-        private void SetTimer(double t) {
-            timer.Stop();
-            timer.Interval = t;
-            timer.Start();
-        }
-
-        public void Reset(double t) {
-            SetTimer(t);
-            startAt = DateTime.UtcNow;
-            max = t;
-        }
-
-        public void Stop() {
-            timer.Stop();
-        }
-
-        public void Extend(double t) {
-            // If the timer's not running, behave like Reset
-            if (!timer.Enabled)
-                Reset(t);
-
-            // If timer is running
-            else {
-                max += t; // extend max
-                SetTimer(max - Current);
-            }
-        }
-
-        /// <summary>Gets how many milliseconds has elapsed since starting timer.</summary>
-        public int Current => (int)(DateTime.UtcNow - startAt).TotalMilliseconds;
-
-        /// <summary>Gets how far through the timer is as a value between 0 and 1 (for use with the fade animation mode).</summary>
-        public double InterpolationValue => Current / max;
-
-        public void Disponse() {
-            timer.Dispose();
-        }
+    public void Dispose() {
+        _timer.Dispose();
     }
+}
     
-    public enum TimerLayerAnimationType {
-        [Description("On/Off")] OnOff,
-        Fade,
-        Progress,
-        [Description("Progress (Gradual)")] ProgressGradual
-    }
+public enum TimerLayerAnimationType {
+    [Description("On/Off")] OnOff,
+    Fade,
+    Progress,
+    [Description("Progress (Gradual)")] ProgressGradual
+}
 
-    public enum TimerLayerRepeatPressAction {
-        [Description("Restart")] Reset,
-        [Description("Extend")] Extend,
-        [Description("Cancel")] Cancel,
-        [Description("Ignore")] Ignore
-    }
+public enum TimerLayerRepeatPressAction {
+    [Description("Restart")] Reset,
+    [Description("Extend")] Extend,
+    [Description("Cancel")] Cancel,
+    [Description("Ignore")] Ignore
 }
