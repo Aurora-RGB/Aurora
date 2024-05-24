d61b47b7ecf8aaeb0f6662b925ed1e43fdd61e14
Aytackydln <aytac.kayadelen@gmail.com>
2024-05-24 15:48:58 +0200

    move ChromaApplication's chroma module initialization to Initialize method for async loading

--- Project-Aurora/Project-Aurora/Profiles/Chroma/ChromaApplication.cs
+++ Project-Aurora/Project-Aurora/Profiles/Chroma/ChromaApplication.cs
@@ -1,30 +1,40 @@
 ﻿using System;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using AuroraRgb.Modules;
+using AuroraRgb.Modules.Razer;
 
 namespace AuroraRgb.Profiles.Chroma;
 
-public sealed class ChromaApplication : Application
+public sealed class ChromaApplication() : Application(new LightEventConfig
 {
-    public ChromaApplication() : base(new LightEventConfig
+    Name = "Chroma Apps",
+    ID = "chroma",
+    ProcessNames = [],
+    ProfileType = typeof(RazerChromaProfile),
+    OverviewControlType = typeof(Control_Chroma),
+    GameStateType = typeof(GameState_Wrapper),
+    IconURI = "Resources/chroma.png",
+    EnableByDefault = true,
+})
+{
+    public override async Task<bool> Initialize(CancellationToken cancellationToken)
     {
-        Name = "Chroma Apps",
-        ID = "chroma",
-        ProcessNames = [],
-        ProfileType = typeof(RazerChromaProfile),
-        OverviewControlType = typeof(Control_Chroma),
-        GameStateType = typeof(GameState_Wrapper),
-        IconURI = "Resources/chroma.png",
-        EnableByDefault = true,
-    })
-    {
-        var chromaRegistrySettings = RazerSdkModule.RzSdkManager.Result.ChromaRegistrySettings;
+        var baseInit = await base.Initialize(cancellationToken);
+
+        var chromaRegistrySettings = await GetChromaRegistrySettings();
         chromaRegistrySettings.ChromaAppsChanged += ChromaRegistrySettingsOnChromaAppsChanged;
         Config.ProcessNames = chromaRegistrySettings.AllChromaApps
             .Where(processName => !string.IsNullOrWhiteSpace(processName))
             .Where(s => !chromaRegistrySettings.ExcludedPrograms.Contains(s))
             .ToArray();
+        return baseInit;
+    }
+
+    private static async Task<ChromaRegistrySettings> GetChromaRegistrySettings()
+    {
+        return (await RazerSdkModule.RzSdkManager).ChromaRegistrySettings;
     }
 
     private async void ChromaRegistrySettingsOnChromaAppsChanged(object? sender, EventArgs e)
@@ -34,7 +44,7 @@ public sealed class ChromaApplication : Application
 
     private async Task SetProfileApplication()
     {
-        var chromaRegistrySettings = (await RazerSdkModule.RzSdkManager).ChromaRegistrySettings;
+        var chromaRegistrySettings = await GetChromaRegistrySettings();
         Config.ProcessNames = chromaRegistrySettings.AllChromaApps
             .Where(processName => !string.IsNullOrWhiteSpace(processName))
             .Where(s => !chromaRegistrySettings.ExcludedPrograms.Contains(s))
@@ -44,7 +54,7 @@ public sealed class ChromaApplication : Application
     public override void Dispose()
     {
         base.Dispose();
-        
+
         RazerSdkModule.RzSdkManager.Result.ChromaRegistrySettings.ChromaAppsChanged -= ChromaRegistrySettingsOnChromaAppsChanged;
     }
 }
