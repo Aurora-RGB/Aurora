498cc2e28ea4b35b07c5fb865ff997bccf9bba39
Aytackydln <aytac.kayadelen@gmail.com>
2024-07-10 23:36:12 +0200

    fix EffectSpeed throwing error if overridden

--- Project-Aurora/Project-Aurora/Settings/Layers/BreathingLayerHandler.cs
+++ Project-Aurora/Project-Aurora/Settings/Layers/BreathingLayerHandler.cs
@@ -9,71 +9,66 @@ using AuroraRgb.Utils;
 using Common.Utils;
 using Newtonsoft.Json;
 
-namespace AuroraRgb.Settings.Layers
+namespace AuroraRgb.Settings.Layers;
+
+public class BreathingLayerHandlerProperties : LayerHandlerProperties2Color<BreathingLayerHandlerProperties>
 {
-    public class BreathingLayerHandlerProperties : LayerHandlerProperties2Color<BreathingLayerHandlerProperties>
+    public bool? _RandomPrimaryColor { get; set; }
+
+    [JsonIgnore]
+    public bool RandomPrimaryColor => Logic?._RandomPrimaryColor ?? _RandomPrimaryColor ?? false;
+
+    public bool? _RandomSecondaryColor { get; set; }
+
+    [JsonIgnore]
+    public bool RandomSecondaryColor => Logic?._RandomSecondaryColor ?? _RandomSecondaryColor ?? false;
+
+    [LogicOverridable("Effect Speed")]
+    public double? _EffectSpeed { get; set; }
+
+    [JsonIgnore]
+    public double EffectSpeed => Logic?._EffectSpeed ?? _EffectSpeed ?? 0.0;
+
+    public BreathingLayerHandlerProperties()
+    { }
+
+    public BreathingLayerHandlerProperties(bool assignDefault = false) : base(assignDefault) { }
+
+    public override void Default()
     {
-        public bool? _RandomPrimaryColor { get; set; }
+        base.Default();
+        _RandomPrimaryColor = false;
+        _RandomSecondaryColor = false;
+        _EffectSpeed = 1.0f;
+    }
+}
 
-        [JsonIgnore]
-        public bool RandomPrimaryColor => Logic?._RandomPrimaryColor ?? _RandomPrimaryColor ?? false;
+public class BreathingLayerHandler() : LayerHandler<BreathingLayerHandlerProperties>("Breathing Layer")
+{
+    private Color _currentPrimaryColor = Color.Transparent;
+    private Color _currentSecondaryColor = Color.Transparent;
 
-        public bool? _RandomSecondaryColor { get; set; }
-
-        [JsonIgnore]
-        public bool RandomSecondaryColor => Logic?._RandomSecondaryColor ?? _RandomSecondaryColor ?? false;
-
-        [LogicOverridable("Effect Speed")]
-        public float? _EffectSpeed { get; set; }
-
-        [JsonIgnore]
-        public float EffectSpeed => Logic?._EffectSpeed ?? _EffectSpeed ?? 0.0f;
-
-        public BreathingLayerHandlerProperties()
-        { }
-
-        public BreathingLayerHandlerProperties(bool assign_default = false) : base(assign_default) { }
-
-        public override void Default()
-        {
-            base.Default();
-            _RandomPrimaryColor = false;
-            _RandomSecondaryColor = false;
-            _EffectSpeed = 1.0f;
-        }
+    protected override UserControl CreateControl()
+    {
+        return new Control_BreathingLayer(this);
     }
 
-    public class BreathingLayerHandler : LayerHandler<BreathingLayerHandlerProperties>
+    public override EffectLayer Render(IGameState gameState)
     {
-        private Color _currentPrimaryColor = Color.Transparent;
-        private Color _currentSecondaryColor = Color.Transparent;
+        var currentSine = (float)Math.Pow(Math.Sin((double)(Time.GetMillisecondsSinceEpoch() % 10000L / 10000.0f) * 2 * Math.PI * Properties.EffectSpeed), 2);
 
-        public BreathingLayerHandler() : base("Breathing Layer")
-        {
-        }
+        if (currentSine <= 0.0025f * Properties.EffectSpeed && Properties.RandomSecondaryColor)
+            _currentSecondaryColor = CommonColorUtils.GenerateRandomColor();
+        else if(!Properties.RandomSecondaryColor)
+            _currentSecondaryColor = Properties.SecondaryColor;
 
-        protected override UserControl CreateControl()
-        {
-            return new Control_BreathingLayer(this);
-        }
+        if (currentSine >= 1.0f - 0.0025f * Properties.EffectSpeed && Properties.RandomPrimaryColor)
+            _currentPrimaryColor = CommonColorUtils.GenerateRandomColor();
+        else if (!Properties.RandomPrimaryColor)
+            _currentPrimaryColor = Properties.PrimaryColor;
 
-        public override EffectLayer Render(IGameState gamestate)
-        {
-            var currentSine = (float)Math.Pow(Math.Sin((double)(Time.GetMillisecondsSinceEpoch() % 10000L / 10000.0f) * 2 * Math.PI * Properties.EffectSpeed), 2);
+        EffectLayer.Set(Properties.Sequence, ColorUtils.BlendColors(_currentPrimaryColor, _currentSecondaryColor, currentSine));
 
-            if (currentSine <= 0.0025f * Properties.EffectSpeed && Properties.RandomSecondaryColor)
-                _currentSecondaryColor = CommonColorUtils.GenerateRandomColor();
-            else if(!Properties.RandomSecondaryColor)
-                _currentSecondaryColor = Properties.SecondaryColor;
-
-            if (currentSine >= 1.0f - 0.0025f * Properties.EffectSpeed && Properties.RandomPrimaryColor)
-                _currentPrimaryColor = CommonColorUtils.GenerateRandomColor();
-            else if (!Properties.RandomPrimaryColor)
-                _currentPrimaryColor = Properties.PrimaryColor;
-
-            EffectLayer.Set(Properties.Sequence, ColorUtils.BlendColors(_currentPrimaryColor, _currentSecondaryColor, currentSine));
-
-            return EffectLayer;
-        }
+        return EffectLayer;
     }
 }
