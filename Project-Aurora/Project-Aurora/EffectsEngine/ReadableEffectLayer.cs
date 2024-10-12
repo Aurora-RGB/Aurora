d965b59051f09627b76545e8ba70894cb4a58c24
Aytackydln <aytac.kayadelen@gmail.com>
2024-10-13 00:01:13 +0200

    remove unnecessary _keyColors map from ReadableEffectLayer.cs

--- Project-Aurora/Project-Aurora/EffectsEngine/ReadableEffectLayer.cs
+++ Project-Aurora/Project-Aurora/EffectsEngine/ReadableEffectLayer.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using AuroraRgb.Bitmaps;
 using Common.Devices;
 
@@ -7,8 +6,6 @@ namespace AuroraRgb.EffectsEngine;
 
 public class ReadableEffectLayer(string name, Color color) : EffectLayer(name, color, LayerReadability.Readable)
 {
-    private readonly Dictionary<DeviceKeys, Color> _keyColors = new(Effects.MaxDeviceId);
-    
     private IBitmapReader? _bitmapReader;
 
     /// <summary>
@@ -16,11 +13,6 @@ public class ReadableEffectLayer(string name, Color color) : EffectLayer(name, c
     /// </summary>
     public Color Get(DeviceKeys key)
     {
-        if (_keyColors.TryGetValue(key, out var color))
-        {
-            return color;
-        }
-
         var keyRectangle = Effects.Canvas.GetRectangle(key);
 
         var keyColor = keyRectangle.IsEmpty switch
@@ -28,7 +20,6 @@ public class ReadableEffectLayer(string name, Color color) : EffectLayer(name, c
             true => Color.Black,
             false => GetColor(keyRectangle.Rectangle)
         };
-        _keyColors[key] = keyColor;
         return keyColor;
     }
 
@@ -36,35 +27,6 @@ public class ReadableEffectLayer(string name, Color color) : EffectLayer(name, c
     {
         _bitmapReader ??= _colormap.CreateReader();
         return _bitmapReader.GetRegionColor(rectangle);
-    }
-
-    public override void Invalidate()
-    {
-        base.Invalidate();
-
-        _keyColors.Clear();
-    }
-
-    protected override void SetOneKey(DeviceKeys key, Brush brush)
-    {
-        if (brush is SolidBrush solidBrush)
-        {
-            if (_keyColors.TryGetValue(key, out var currentColor) && currentColor.ToArgb() == solidBrush.Color.ToArgb())
-            {
-                return;
-            }
-
-            _keyColors[key] = solidBrush.Color;
-        }
-
-        base.SetOneKey(key, brush);
-    }
-
-    public override void Dispose()
-    {
-        base.Dispose();
-        
-        _keyColors.Clear();
     }
 
     public void Close()
