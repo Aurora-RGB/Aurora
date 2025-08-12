31de034fbdec022d57b47a26d4b5f8a7915057a9
Aytackydln <aytac.kayadelen@gmail.com>
2025-08-12 20:34:49 +0200

    add button pressed properties to controller node

--- Project-Aurora/Project-Aurora/Nodes/ControllerNode.cs
+++ Project-Aurora/Project-Aurora/Nodes/ControllerNode.cs
@@ -28,4 +28,25 @@ public class ControllerNode(int userIndex) : Node
 
     public short RightThumbX => XInput.GetState(userIndex, out _state) ? _state.Gamepad.RightThumbX : (short)0;
     public short RightThumbY => XInput.GetState(userIndex, out _state) ? _state.Gamepad.RightThumbY : (short)0;
+    
+    public bool IsLeftThumbPressed => Buttons.HasFlag(GamepadButtons.LeftThumb);
+    public bool IsRightThumbPressed => Buttons.HasFlag(GamepadButtons.RightThumb);
+    public bool IsLeftShoulderPressed => Buttons.HasFlag(GamepadButtons.LeftShoulder);
+    public bool IsRightShoulderPressed => Buttons.HasFlag(GamepadButtons.RightShoulder);
+    
+    public bool IsAButtonPressed => Buttons.HasFlag(GamepadButtons.A);
+    public bool IsBButtonPressed => Buttons.HasFlag(GamepadButtons.B);
+    public bool IsXButtonPressed => Buttons.HasFlag(GamepadButtons.X);
+    public bool IsYButtonPressed => Buttons.HasFlag(GamepadButtons.Y);
+    
+    public bool IsDPadUpPressed => Buttons.HasFlag(GamepadButtons.DPadUp);
+    public bool IsDPadDownPressed => Buttons.HasFlag(GamepadButtons.DPadDown);
+    public bool IsDPadLeftPressed => Buttons.HasFlag(GamepadButtons.DPadLeft);
+    public bool IsDPadRightPressed => Buttons.HasFlag(GamepadButtons.DPadRight);
+    
+    public bool IsStartButtonPressed => Buttons.HasFlag(GamepadButtons.Start);
+    public bool IsBackButtonPressed => Buttons.HasFlag(GamepadButtons.Back);
+    public bool IsGuideButtonPressed => Buttons.HasFlag(GamepadButtons.Guide);
+    
+    private GamepadButtons Buttons => XInput.GetState(userIndex, out _state) ? _state.Gamepad.Buttons : GamepadButtons.None;
 }
