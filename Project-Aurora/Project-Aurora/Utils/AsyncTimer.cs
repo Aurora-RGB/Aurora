7f27acf45924bf38619b80c0ba2ea9521aebdbeb
Aytackydln <aytac.kayadelen@gmail.com>
2026-02-21 18:29:26 +0100

    use AsyncTimer for Temporary<> timer and fix early disposing with thread starvations

---
+++ Project-Aurora/Project-Aurora/Utils/AsyncTimer.cs
@@ -0,0 +1,58 @@
+﻿namespace AuroraRgb.Utils;
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+public sealed class AsyncTimer : IAsyncDisposable
+{
+    private readonly Func<CancellationToken, Task> _callback;
+    private readonly TimeSpan _period;
+    private readonly CancellationTokenSource _cts = new();
+    private readonly Task? _runningTask;
+
+    public AsyncTimer(Func<CancellationToken, Task> callback, TimeSpan period)
+    {
+        _callback = callback ?? throw new ArgumentNullException(nameof(callback));
+        _period = period;
+
+        // Start the timer loop
+        _runningTask = TimerLoopAsync(_cts.Token);
+    }
+
+    private async Task TimerLoopAsync(CancellationToken token)
+    {
+        try
+        {
+            while (!token.IsCancellationRequested)
+            {
+                await Task.Delay(_period, token);
+                try
+                {
+                    await _callback(token);
+                }
+                catch (OperationCanceledException) when (token.IsCancellationRequested)
+                {
+                    // Timer was canceled, exit gracefully
+                }
+                catch (Exception ex)
+                {
+                    // Log or handle callback exceptions
+                    Global.logger.Warning(ex, "Exception while executing callback");
+                }
+            }
+        }
+        catch (OperationCanceledException)
+        {
+            // Timer canceled
+        }
+    }
+
+    public ValueTask DisposeAsync()
+    {
+        // don't block since this will be called from timer callback itself
+        _ = _cts.CancelAsync();
+        _cts.Dispose();
+        return ValueTask.CompletedTask;
+    }
+}
--- Project-Aurora/Project-Aurora/Utils/Temporary.cs
+++ Project-Aurora/Project-Aurora/Utils/Temporary.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Nito.AsyncEx;
 
 namespace AuroraRgb.Utils;
 
@@ -10,7 +12,7 @@ public sealed class Temporary<T>(Func<T> produce, bool callDispose = true) : IDi
 {
     public event EventHandler? ValueCreated;
 
-    private readonly object _createLock = new();
+    private readonly Lock _createLock = new();
     private volatile T? _value;
 
     private DateTime _lastAccess = DateTime.UtcNow;
@@ -28,18 +30,22 @@ public sealed class Temporary<T>(Func<T> produce, bool callDispose = true) : IDi
                 return _value;
             }
 
-            lock(_createLock)
+            lock (_createLock)
             {
+                // prevent deletions with thread starvation
+                using var readLock = TimerLock.WriterLock();
+
                 if (_value != null)
                 {
                     return _value;
                 }
 
-                //TODO add an infinite loop check
-                var value = produce.Invoke();
+                var value = produce();
                 _value = value;
                 ValueCreated?.Invoke(this, EventArgs.Empty);
                 AddInstance(this);
+
+                _lastAccess = DateTime.UtcNow;
                 return value;
             }
         }
@@ -63,6 +69,7 @@ public sealed class Temporary<T>(Func<T> produce, bool callDispose = true) : IDi
         {
             disposable.Dispose();
         }
+
         _value = null;
     }
 
@@ -92,39 +99,42 @@ public sealed class Temporary<T>(Func<T> produce, bool callDispose = true) : IDi
     #region static
 
     private static readonly List<Temporary<T>> Instances = [];
+
     // ReSharper disable once StaticMemberInGenericType
-    private static Timer? _aliveTimer;
+    private static AsyncTimer? _aliveTimer;
+
     // ReSharper disable once StaticMemberInGenericType
-    private static readonly ReaderWriterLockSlim TimerLock = new();
+    private static readonly AsyncReaderWriterLock TimerLock = new();
 
     private static void AddInstance(Temporary<T> temporary)
     {
-        TimerLock.EnterWriteLock();
         Instances.Add(temporary);
         _aliveTimer ??= StartAliveTimer();
-        TimerLock.ExitWriteLock();
     }
 
-    private static Timer StartAliveTimer()
+    private static AsyncTimer StartAliveTimer()
     {
-        return new Timer(AliveTimerCallback, null, TimeSpan.FromSeconds(20), TimeSpan.FromSeconds(20));
+        return new AsyncTimer(AliveTimerCallback, TimeSpan.FromSeconds(20));
     }
 
-    private static void AliveTimerCallback(object? state)
+    private static async Task AliveTimerCallback(CancellationToken cancellationToken)
     {
-        TimerLock.EnterWriteLock();
+        using var writeLock = await TimerLock.WriterLockAsync(cancellationToken);
+
+        // delay the deletion a bit if threads are starved
+        await Task.Yield();
 
         Instances.RemoveAll(ExpiredInstance);
         if (Instances.Count != 0)
         {
-            TimerLock.ExitWriteLock();
             return;
         }
 
-        _aliveTimer?.Dispose();
+        if (_aliveTimer != null)
+        {
+            await _aliveTimer.DisposeAsync();
+        }
         _aliveTimer = null;
-
-        TimerLock.ExitWriteLock();
     }
 
     private static bool ExpiredInstance(Temporary<T> temporary)
