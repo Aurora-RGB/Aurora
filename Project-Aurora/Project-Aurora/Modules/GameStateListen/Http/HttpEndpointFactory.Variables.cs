03ad1a3c8bd671e37e5095e1f348b7fca8970215
Aytackydln <aytac.kayadelen@gmail.com>
2026-02-26 18:20:03 +0100

    add query and body form data processing for global variables HTTP endpoint

--- Project-Aurora/Project-Aurora/Modules/GameStateListen/Http/HttpEndpointFactory.Variables.cs
+++ Project-Aurora/Project-Aurora/Modules/GameStateListen/Http/HttpEndpointFactory.Variables.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.IO;
 using System.Net;
 using System.Text.Json;
 using AuroraRgb.Nodes;
@@ -35,50 +34,116 @@ public static partial class HttpEndpointFactory
         void ProcessPostVariables(HttpContext context)
         {
             var request = context.Request;
-            var inputStream = request.Body;
-            // low mem-alloc process
-            var jsonNode = JsonSerializer.Deserialize<JsonElement>(inputStream);
-
-            // immediately respond, don't let it wait for response
-            CloseConnection(context.Response);
-
-            switch (jsonNode.ValueKind)
+            if (request.Query.Count > 0)
             {
-                case JsonValueKind.Object:
-                    foreach (var property in jsonNode.EnumerateObject())
+                ProcessQueryString(context);
+            }
+            
+            var contentType = request.ContentType;
+            if (contentType == null || contentType.StartsWith("application/json", StringComparison.OrdinalIgnoreCase))
+            {
+                if (request.ContentLength is > 2)
+                {
+                    ProcessJsonInput(request, context);
+                }
+            }
+            else if (contentType.StartsWith("application/x-www-form-urlencoded", StringComparison.OrdinalIgnoreCase))
+            {
+                ProcessBodyFormData(context);
+            }
+            else
+            {
+                context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+        }
+    }
+
+    private static void ProcessJsonInput(HttpRequest request, HttpContext context)
+    {
+        var inputStream = request.Body;
+        // low mem-alloc process
+        var jsonNode = JsonSerializer.Deserialize<JsonElement>(inputStream);
+
+        // immediately respond, don't let it wait for response
+        CloseConnection(context.Response);
+
+        var auroraVariables = AuroraVariables.Instance;
+        switch (jsonNode.ValueKind)
+        {
+            case JsonValueKind.Object:
+                foreach (var property in jsonNode.EnumerateObject())
+                {
+                    var key = property.Name;
+                    var value = property.Value;
+                    switch (value.ValueKind)
                     {
-                        var key = property.Name;
-                        var value = property.Value;
-                        switch (value.ValueKind)
-                        {
-                            case JsonValueKind.String:
-                                var stringValue = value.GetString();
-                                if (stringValue == null)
-                                {
-                                    AuroraVariables.Instance.Strings.Remove(key);
-                                }
-                                else
-                                {
-                                    AuroraVariables.Instance.Strings[key] = stringValue;
-                                }
-
-                                break;
-                            case JsonValueKind.Number:
-                                AuroraVariables.Instance.Numbers[key] = value.GetDouble();
-                                break;
-                            case JsonValueKind.True:
-                            case JsonValueKind.False:
-                                AuroraVariables.Instance.Booleans[key] = value.GetBoolean();
-                                break;
-                            case JsonValueKind.Null:
-                                AuroraVariables.Instance.Strings.Remove(key);
-                                AuroraVariables.Instance.Numbers.Remove(key);
-                                AuroraVariables.Instance.Booleans.Remove(key);
-                                break;
-                        }
+                        case JsonValueKind.String:
+                            var stringValue = value.GetString()!; 
+                            auroraVariables.Strings[key] = stringValue;
+                            break;
+                        case JsonValueKind.Number:
+                            auroraVariables.Numbers[key] = value.GetDouble();
+                            break;
+                        case JsonValueKind.True:
+                            auroraVariables.Booleans[key] = true;
+                            break;
+                        case JsonValueKind.False:
+                            auroraVariables.Booleans[key] = false;
+                            break;
+                        case JsonValueKind.Null:
+                            auroraVariables.Strings.Remove(key);
+                            auroraVariables.Numbers.Remove(key);
+                            auroraVariables.Booleans.Remove(key);
+                            break;
                     }
+                }
 
-                    break;
+                break;
+        }
+    }
+
+    private static void ProcessBodyFormData(HttpContext context)
+    {
+        var request = context.Request;
+        var form = request.Form;
+        var auroraVariables = AuroraVariables.Instance;
+        foreach (var (key, value) in form)
+        {
+            // try parse as number
+            if (double.TryParse(value, out var numberValue))
+            {
+                auroraVariables.Numbers[key] = numberValue;
+            }
+            else if (bool.TryParse(value, out var boolValue))
+            {
+                auroraVariables.Booleans[key] = boolValue;
+            }
+            else
+            {
+                auroraVariables.Strings[key] = value;
+            }
+        }
+    }
+    
+    private static void ProcessQueryString(HttpContext context)
+    {
+        var request = context.Request;
+        var query = request.Query;
+        var auroraVariables = AuroraVariables.Instance;
+        foreach (var (key, value) in query)
+        {
+            // try parse as number
+            if (double.TryParse(value, out var numberValue))
+            {
+                auroraVariables.Numbers[key] = numberValue;
+            }
+            else if (bool.TryParse(value, out var boolValue))
+            {
+                auroraVariables.Booleans[key] = boolValue;
+            }
+            else
+            {
+                auroraVariables.Strings[key] = value;
             }
         }
     }
